17412655ea7a1a19eedf6c71b359a672393b1b30
Aaron Bush <89756454+Awesomecake@users.noreply.github.com>
2024-04-06 20:51:09 -0400

    Added Basic Main Menu

---
+++ Wheatley/Assets/Scripts/ManageScenes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameplayScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
