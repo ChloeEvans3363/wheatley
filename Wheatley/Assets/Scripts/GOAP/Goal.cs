54bfb120cb903c08780dc5be54d33904da25c26e
Chloe <cre3363@g.rit.edu>
2024-04-12 02:58:11 -0400

    Just did some file adding and organizing in preparation for GOAP

--- Wheatley/Assets/Scripts/GOAP/Goal.cs
+++ Wheatley/Assets/Scripts/GOAP/Goal.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RAT : MonoBehaviour
+public class Goal : MonoBehaviour
 {
-    // This class is going to contain the finite state machine
-    // That switches between actions and moving
-
-    public enum AIStates
-    {
-        Move,
-        Action
-    }
-
     // Start is called before the first frame update
     void Start()
     {
