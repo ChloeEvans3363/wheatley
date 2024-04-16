f39f7ec78d4b2897f405f55e3aefbd4181ca2632
Chloe <cre3363@g.rit.edu>
2024-04-16 01:09:05 -0400

    Cleaned up some more code

--- Wheatley/Assets/Scripts/GOAP/Actions/A_MoveBlock.cs
+++ Wheatley/Assets/Scripts/GOAP/Actions/A_MoveBlock.cs
@@ -22,14 +22,14 @@ public class A_MoveBlock : Action
         {
             Vector2 playerPositon = new Vector2(state.playerTile.transform.position.x, state.playerTile.transform.position.z);
 
-            foreach(GameObject block in state.moveableBlocks.Values)
+            foreach(var bKey in state.moveableBlocks.Keys)
             {
                 foreach(GameObject pit in state.pits.Values)
                 {
                     Vector2 endPos = new Vector2(pit.transform.position.x, pit.transform.position.z);
-                    Vector2 cratePos = new Vector2(block.transform.position.x, block.transform.position.z);
+                    Vector2 cratePos = new Vector2(state.objectsPositions[bKey].x, state.objectsPositions[bKey].z);
 
-                    if(PushBoxSearch.CanPushBox(map.mapHeights, playerPositon, cratePos, endPos))
+                    if (PushBoxSearch.CanPushBox(map.mapHeights, playerPositon, cratePos, endPos))
                     {
                         return true;
                     }
@@ -47,12 +47,12 @@ public class A_MoveBlock : Action
         {
             Vector2 playerPositon = new Vector2(state.playerTile.transform.position.x, state.playerTile.transform.position.z);
 
-            foreach (GameObject block in state.moveableBlocks.Values)
+            foreach (var bKey in state.moveableBlocks.Keys)
             {
                 foreach (GameObject pit in state.pits.Values)
                 {
                     Vector2 endPos = new Vector2(pit.transform.position.x, pit.transform.position.z);
-                    Vector2 cratePos = new Vector2(block.transform.position.x, block.transform.position.z);
+                    Vector2 cratePos = new Vector2(state.objectsPositions[bKey].x, state.objectsPositions[bKey].z);
 
                     if (PushBoxSearch.CanPushBox(map.mapHeights, playerPositon, cratePos, endPos))
                     {
@@ -84,8 +84,9 @@ public class A_MoveBlock : Action
                     {
                         Vector2 endPos = new Vector2(successorState.pits[pKey].transform.position.x,
                             successorState.pits[pKey].transform.position.z);
-                        Vector2 cratePos = new Vector2(successorState.moveableBlocks[bKey].transform.position.x,
-                            successorState.moveableBlocks[bKey].transform.position.z);
+
+                        Vector2 cratePos = new Vector2(successorState.objectsPositions[bKey].x,
+                            successorState.objectsPositions[bKey].z);
 
                         if (PushBoxSearch.CanPushBox(map.mapHeights, playerPositon, cratePos, endPos))
                         {
